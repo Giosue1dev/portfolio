3744f13e4662a2bb5c0ecb6416f3dacb67b12769
Giosue1dev <131166708+Giosue1dev@users.noreply.github.com>
2023-05-25 15:01:54 +0200

    progect js

---
+++ Progetto js/js/script.js
@@ -0,0 +1,39 @@
+let buttonPlus = document.querySelector("plus");
+let buttonReset = document.querySelector("reset");
+let buttonLess = document.querySelector("less");
+var counter = 0;
+
+
+plus.onclick = function(){
+counter++;
+document.getElementById("result").innerHTML=counter;
+} 
+reset.onclick = function(){
+if (counter!=0) {
+counter=0;
+} else {
+alert("sei gia a zero");
+}
+document.getElementById("result").innerHTML=counter;
+}
+less.onclick = function(){
+if(counter!=0){
+counter--;
+}else{
+alert("non puoi scendere sotto lo 0");
+}
+document.getElementById("result").innerHTML=counter;
+}
+
+function showTime(){
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    document.getElementById("hour").innerHTML=hours;
+    document.getElementById("minutes").innerHTML=minutes;
+    document.getElementById("seconds").innerHTML=seconds;
+}
+
+setInterval(showTime,10);
